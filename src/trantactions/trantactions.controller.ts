8fe821e5e5a9d4680d29e3459f5a1ca441a259b6
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-07-16 08:27:47 -0500

    feat(transactions): :fire: Create transaction endpoint finished

--- src/trantactions/trantactions.controller.ts
+++ src/trantactions/trantactions.controller.ts
@@ -1,3 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { TrantactionsService } from './trantactions.service';
+import { CreateTrantactionDto } from './dto/create-trantaction.dto';
 @Controller('trantactions')
-export class TrantactionsController {}
+export class TrantactionsController {
+  constructor(private readonly transactionsService: TrantactionsService) {}
+
+  @Post()
+  create(@Body() createTransactionsDto: CreateTrantactionDto) {
+    return this.transactionsService.create(createTransactionsDto);
+  }
+}
--- src/trantactions/trantactions.service.ts
+++ src/trantactions/trantactions.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, Transaction } from 'typeorm';
+import { CreateTrantactionDto } from './dto/create-trantaction.dto';
+import { Trantaction } from './entities/trantaction.entity';
 
 @Injectable()
-export class TrantactionsService {}
+export class TrantactionsService {
+  private logger = new Logger('transactionsService');
+
+  constructor(
+    @InjectRepository(Transaction)
+    private readonly transactionsRepository: Repository<Trantaction>,
+  ) {}
+
+  async create(createTrantactionDto: CreateTrantactionDto) {
+    try {
+      const transaction =
+        await this.transactionsRepository.save(createTrantactionDto);
+
+      return transaction;
+    } catch (error) {
+      this.logger.error(error);
+      throw new InternalServerErrorException('Something went worng!');
+    }
+  }
+}
