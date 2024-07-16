18aedfbb16867e8cf25b277c22be0a4576a9477a
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-07-16 08:18:27 -0500

    feat(transactions): :fire: Finished create transaction DTO

--- src/trantactions/dto/create-trantaction.dto.ts
+++ src/trantactions/dto/create-trantaction.dto.ts
@@ -1,1 +1,15 @@
-export class CreateTrantactionDto {}
+import { IsDate, IsPositive, IsString } from 'class-validator';
+
+export class CreateTrantactionDto {
+  @IsDate()
+  created_at: Date;
+
+  @IsPositive()
+  amount_in_cents: number;
+
+  @IsString()
+  status: string;
+
+  @IsString()
+  payment_method_type: string;
+}
