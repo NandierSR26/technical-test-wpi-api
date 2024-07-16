a821e10a56704c6697e26a545ce6895420fe17e4
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-07-16 07:33:07 -0500

    feat(transactions): :zap: Created transactions entity

--- src/trantactions/entities/trantaction.entity.ts
+++ src/trantactions/entities/trantaction.entity.ts
@@ -1,1 +1,19 @@
-export class Trantaction {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('transactions')
+export class Trantaction {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('timestamp')
+  created_at: Date;
+
+  @Column('int')
+  amount_in_cents: number;
+
+  @Column('text')
+  status: string;
+
+  @Column('text')
+  payment_method_type: string;
+}
